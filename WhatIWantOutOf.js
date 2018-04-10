a483951c5da5c3bb3708ba3c5639d8ced113ddf6
ottovbismarck <fritzj96@gmail.com>
2018-04-09 18:40:29 -0700

    Only Commit

---
+++ WhatIWantOutOf.js
@@ -0,0 +1,1 @@
+console.log("I'm here because I want to show Geekwise/Bitwise higher-ups that I can be patient & flexible with demands. I hope to learn a little about JS but mostly the soft skills.")
